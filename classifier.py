8090361643807751b21261920c195fd7fce4cd59
ss4936 <shahsahilj@gmail.com>
2016-05-10 15:31:03 -0400

    precision score

--- classifier.py
+++ classifier.py
@@ -13,11 +13,12 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier 
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
 
 
 logging.captureWarnings(True)
 
-with open('g1.txt') as datafile:
+with open('g2.txt') as datafile:
     data=json.load(datafile)
 count_vectorizer = CountVectorizer()
 jdata=json.dumps(data)
@@ -35,9 +36,9 @@ def features(d):
     termsText = tokenize(d['reviewText'])
     termsSummary = tokenize(d['summary'])
     data = {'overall': float(d['overall']), 'unixReviewTime': float(d['unixReviewTime'])}
-##    data['count']=float(d['count'])
-##    data['text_length']=len(termsText)
-##    data['summary_length']=len(termsSummary)
+    data['count']=float(d['count'])
+    data['text_length']=len(termsText)
+    data['summary_length']=len(termsSummary)
     for t in termsText:
         data[t] = data.get(t, 0) + 1
     for t in termsSummary:
@@ -54,29 +55,33 @@ train_targets = trainpdata['hfactor'].values
 
 test_targets = testpdata['hfactor'].values
 
-print 'Grocery reviews'
+print 'Grocery and Gourmet Foods reviews\n'
 
 classifier_NB = MultinomialNB()
 classifier_NB.fit(X_train, train_targets)
 predictions_NB = classifier_NB.predict(X_test)
 print 'Naive Bayes'
-print accuracy_score(test_targets,predictions_NB)
+print 'Accuracy score: '+str(accuracy_score(test_targets,predictions_NB))
+print 'Precision score: '+str(precision_score(test_targets,predictions_NB))
     
 model_LR = LogisticRegression()
 model_LR.fit(X_train, train_targets)
 predictions_LR = model_LR.predict(X_test)
 print 'Logistic Regression'
-print accuracy_score(test_targets,predictions_LR)
+print 'Accuracy score: '+str(accuracy_score(test_targets,predictions_LR))
+print 'Precision score: '+str(precision_score(test_targets,predictions_LR))
 
 modelTree = DecisionTreeClassifier(max_depth=7)
 modelTree.fit(X_train, train_targets)
 predictions_Tree = modelTree.predict(X_test)
 print 'Decision Tree'
-print accuracy_score(test_targets,predictions_Tree)
+print 'Accuracy score: '+str(accuracy_score(test_targets,predictions_Tree))
+print 'Precision score: '+str(precision_score(test_targets,predictions_Tree))
 
 
 modelGr = GradientBoostingClassifier()
 modelGr.fit(X_train, train_targets)
 predictions_Gr = np.array(modelGr.predict(X_test.toarray()))
 print 'Gradient Boosting'
-print accuracy_score(test_targets,predictions_Gr)
+print 'Accuracy score: '+str(accuracy_score(test_targets,predictions_Gr))
+print 'Precision score: '+str(precision_score(test_targets,predictions_Gr))
